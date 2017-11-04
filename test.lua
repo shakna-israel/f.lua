4e790fd54c3bf9069ca3ae44e434710dc6c8f7d6
James Milne <james.milne@protonmail.com>
2017-11-04 16:17:28 +1100

    f.cons tests

--- test.lua
+++ test.lua
@@ -1,1 +1,12 @@
 f = require "f"
+
+-- f.cons(1, nil)
+assert(type(f.cons(1)) == "table")
+assert(f.cons(1)[1] == 1)
+assert(#f.cons(1) == 1)
+
+-- f.cons(1, table)
+assert(type(f.cons(1, {2})) == "table")
+assert(f.cons(1, {2})[1] == 1)
+assert(f.cons(1, {2})[2] == 2)
+assert(#f.cons(1, {2}) == 2)
